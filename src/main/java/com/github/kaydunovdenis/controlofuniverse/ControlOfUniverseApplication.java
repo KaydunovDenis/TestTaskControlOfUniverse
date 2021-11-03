14dddec21346cf35a3a2be1bb315acc032431d8f
KaydunovDenis <deniskaydunov@gmail.com>
2021-11-03 16:50:54 +0300

    init commit

---
+++ src/main/java/com/github/kaydunovdenis/controlofuniverse/ControlOfUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.github.kaydunovdenis.controlofuniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlOfUniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlOfUniverseApplication.class, args);
+    }
+
+}
